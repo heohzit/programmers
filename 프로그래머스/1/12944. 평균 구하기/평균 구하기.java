43b3382242c9d5b3a0c724273190102ff436e540
heohzit <142005775+heohzit@users.noreply.github.com>
2023-12-04 16:47:43 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        double answer1 = 0;
+        for(int i=0;i<arr.length;i++){
+            answer1+=arr[i];
+            answer=answer1/arr.length;
+            
+        }
+        return answer;
+    }
+}
