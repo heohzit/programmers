edca8a999a8904edca1386bd06393e7bd9a80c80
heohzit <142005775+heohzit@users.noreply.github.com>
2023-12-05 13:50:43 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        for(long i=0;i*i<=n;i++){
+            if(i*i==n){
+                answer =(i+1)*(i+1);
+            }
+        }
+        return answer ==0?-1:answer;
+    }
+}
