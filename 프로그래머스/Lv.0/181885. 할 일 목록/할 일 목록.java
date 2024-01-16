3e2739f4bf60ae6c4f615d7a824191cb74bc9a48
heohzit <142005775+heohzit@users.noreply.github.com>
2024-01-16 15:47:36 +0900

    [level Lv.0] Title: 할 일 목록, Time: 0.02 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public List solution(String[] todo_list, boolean[] finished) {
+        List<String> answer = new ArrayList<>();
+        for(int i=0;i<finished.length;i++){
+            if(finished[i]==false){
+               answer.add(todo_list[i]);
+            }
+        }
+        return answer;
+    }
+}
