b50c8e8941ff0c79623e08e6e82e8b99d23498b2
heohzit <142005775+heohzit@users.noreply.github.com>
2024-02-03 16:42:10 +0900

    [level 0] Title: 0 떼기, Time: 6.77 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -2,6 +2,7 @@ class Solution {
     public String solution(String n_str) {
         String answer = "";
         int answer1 = Integer.parseInt(n_str);
+        
         return answer1+"";
     }
 }
